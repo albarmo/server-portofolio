63614d2dead96c0b189a4394f79fe18d0ea95f8b
ghalihanan <82275070+ghalihanan@users.noreply.github.com>
2021-12-23 23:42:26 +0700

    Create and GET collection Controller and Route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRouter = require("./userRouter");
+const collectionRouter = require("./collectionRouter")
 
 router.use("/user", userRouter);
+router.use("/collection",collectionRouter);
 
 module.exports = router;
