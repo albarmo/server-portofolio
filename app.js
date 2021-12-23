b031088dd27bc4b6fbdd4ac49ebe76095d072034
albarm <albar@feedloop.io>
2021-12-24 01:47:49 +0700

    added husky and lint-stagged

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ App.get("/", (req, res) => {
   .json({ ServerStatus: `Running` });
 });
 
-console.log('blaballasadg')
+console.log('husky should check this')
 
 App.listen(port, () => {
   console.log(`This Server running on port ${port}`);
