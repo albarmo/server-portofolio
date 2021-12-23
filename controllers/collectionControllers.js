7e31b493e2cbad16a534f1d9b0ce88ed361ce656
albarm <albar@feedloop.io>
2021-12-24 01:22:03 +0700

    add precommit eslint js linter airbnb and pretier and precommit husky

--- controllers/collectionControllers.js
+++ controllers/collectionControllers.js
@@ -1,34 +1,31 @@
-const db = require("../models");
+const db = require('../models');
 const Collection = db.Collection;
 const Op = db.Sequelize.Op;
-
 
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: 'Content can not be empty!',
     });
     return;
   }
 
-
   const collection = {
     title: req.body.title,
     productId: req.body.productId,
-    image: req.body.image ? req.body.image : false
+    image: req.body.image ? req.body.image : false,
   };
 
   Collection.create(collection)
-  .then(data => {
-    res.send(data);
-  })
-  .catch(err => {
-    res.status(500).send({
-      message:
-        err.message || "Some error occurred while creating the Col."
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while creating the Col.',
+      });
     });
-  });
 };
 
 exports.findAll = (req, res) => {
@@ -36,37 +33,32 @@ exports.findAll = (req, res) => {
   var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
 
   Collection.findAll({ where: condition })
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving Coll."
+        message: err.message || 'Some error occurred while retrieving Coll.',
       });
     });
 };
 
+// exports.findOne = (request, res) => {
 
-// exports.findOne = (req, res) => {
-  
 // };
-
 
 // exports.update = (req, res) => {
-  
+
 // };
 
-
 // exports.delete = (req, res) => {
-  
+
 // };
 
 // exports.deleteAll = (req, res) => {
-  
+
 // };
 
-
 // exports.findAllPublished = (req, res) => {
-  
+
 // };
