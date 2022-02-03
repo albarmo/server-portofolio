8dcf2d168ff21ce25a6f5d2937ca421fe21004e8
albarm <albar@feedloop.io>
2022-02-03 23:34:21 +0700

    feat: Define the static file path

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ App.get('/', (req, res) => {
   return res.status(200).json({ ServerStatus: `Running` });
 });
 
+// Define the static file path
+App.use(express.static(__dirname + '/public/uploads'));
+App.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
 App.listen(port, () => {
   console.log(`This Server running on port ${port}`);
 });
